793eb32cf5bc9c97c68fc831075dffce2cdad3ca
dnagarajan1 <dnagarajan1@student.gsu.edu>
2026-04-23 15:02:58 -0400

    Added files to the CW5

---
+++ Counter.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    this.incrementCount = this.incrementCount.bind(this);
+  }
+
+  incrementCount() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.count}</p>
+        <button onClick={this.incrementCount}>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
