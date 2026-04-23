5a744ef3e2cb733b6f25c3062e5768caa50b839f
dnagarajan1 <dnagarajan1@student.gsu.edu>
2026-04-23 16:39:49 -0400

    CW5 React app

---
+++ cw5-app/src/components/FilteredList.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { DropdownButton, Dropdown } from 'react-bootstrap';
+import List from './List';
+
+class FilteredList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: "",
+      type: "All"
+    };
+    this.onSearch = this.onSearch.bind(this);
+    this.onFilter = this.onFilter.bind(this);
+  }
+
+  onSearch = (event) => {
+    this.setState({ search: event.target.value });
+  }
+
+  onFilter = (eventKey) => {
+    this.setState({ type: eventKey });
+  }
+
+  filterItem = (item) => {
+    const matchesSearch = item.name
+      .toLowerCase()
+      .search(this.state.search.toLowerCase()) !== -1;
+    const matchesType = this.state.type === "All" || item.type === this.state.type;
+    return matchesSearch && matchesType;
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder="Search..." onChange={this.onSearch} />
+        <DropdownButton title="Filter" onSelect={this.onFilter}>
+          <Dropdown.Item eventKey="All">All</Dropdown.Item>
+          <Dropdown.Item eventKey="Fruit">Fruit</Dropdown.Item>
+          <Dropdown.Item eventKey="Vegetable">Vegetables</Dropdown.Item>
+        </DropdownButton>
+        <List items={this.props.items.filter(this.filterItem)} />
+      </div>
+    );
+  }
+}
+
+export default FilteredList;
